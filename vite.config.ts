df47b7db6c92fa9ae97da0a0a1dbc97def790c5a
Salzmann Sandro ssalzman <sandro.salzmann@stud.hslu.ch>
2023-10-09 21:32:29 +0200

    build: fix hosting in subdirectory

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    base: env.VITE_HOMEPAGE,
+  };
+});
